b33134c8c8b0f5aa6b7e9dad732b38784daa4f7b
Root USO User <student@stud.acs.upb.ro>
2019-03-25 09:25:03 +0200

    implemented taks for laboratory 05

--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
@@ -21,11 +21,20 @@ public class StartedServiceActivity extends AppCompatActivity {
         messageTextView = (TextView)findViewById(R.id.message_text_view);
 
         // TODO: exercise 6 - start the service
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("ro.pub.cs.systems.eim.lab05.startedservice", "ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"));
+        startService(intent);
 
         // TODO: exercise 8a - create an instance of the StartedServiceBroadcastReceiver broadcast receiver
+        startedServiceBroadcastReceiver = new StartedServiceBroadcastReceiver(messageTextView);
+
 
         // TODO: exercise 8b - create an instance of an IntentFilter
         // with all available actions contained within the broadcast intents sent by the service
+        startedServiceIntentFilter = new IntentFilter();
+        startedServiceIntentFilter.addAction(Constants.ACTION_STRING);
+        startedServiceIntentFilter.addAction(Constants.ACTION_INTEGER);
+        startedServiceIntentFilter.addAction(Constants.ACTION_ARRAY_LIST);
 
     }
 
@@ -34,19 +43,22 @@ public class StartedServiceActivity extends AppCompatActivity {
         super.onResume();
 
         // TODO: exercise 8c - register the broadcast receiver with the corresponding intent filter
+        registerReceiver(startedServiceBroadcastReceiver, startedServiceIntentFilter);
     }
 
     @Override
     protected void onPause() {
         // TODO: exercise 8c - unregister the broadcast receiver
-
+        unregisterReceiver(startedServiceBroadcastReceiver);
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
         // TODO: exercise 8d - stop the service
-
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("ro.pub.cs.systems.eim.lab05.startedservice", "ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"));
+        stopService(intent);
         super.onDestroy();
     }
 
